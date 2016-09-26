81d88ac09a7dc24894fb6a91ed6477921f47e255
Peter Tschann <ptgrimm@gmail.com>
2016-09-25 17:24:38 -0700

    this is how you create a new template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users,  controllers: { omniauth_callbacks: "callbacks" }
+  get '/profile' => 'home#profile', as: :profile
   root to: "home#index"
 end
