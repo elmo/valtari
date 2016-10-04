0cfabb889bdb463244d8d71bb9ca79d22de52897
Elliott Blatt <elliott.blatt@gmail.com>
2016-10-03 21:24:06 -0700

    fixed broken link to business_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :businesses
   devise_for :users,  controllers: { omniauth_callbacks: "callbacks" }
   get '/profile' => 'home#profile', as: :profile
   root to: "home#index"
+  resources :businesses, only: [:index, :show]
   namespace :admin do
    resources :businesses
   end
