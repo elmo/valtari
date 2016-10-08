fba62b787f91a32d4ccf4121ac0ae26eb7d3932a
Elliott Blatt <elliott.blatt@gmail.com>
2016-10-08 10:43:27 -0700

    load valtari businesses

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -4,7 +4,7 @@ class BusinessesController < ApplicationController
   # GET /businesses
   # GET /businesses.json
   def index
-    @businesses = Business.all
+    @businesses = Business.all.page(params[:page]).per(10)
   end
 
   # GET /businesses/1
