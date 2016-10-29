fd813adf82bf037768e4959399b3526d612737eb
Peter Tschann <ptgrimm@gmail.com>
2016-10-29 10:52:55 -0700

    cleaned up new capital form a little

--- app/assets/javascripts/valtari.js
+++ app/assets/javascripts/valtari.js
@@ -2,19 +2,16 @@ $(document).on('turbolinks:load', function() {
  jQuery(".best_in_place").best_in_place();
 
  	//Max characters in decimal input
-	var max_chars = 4;
-
-	$('input.decimal').keydown( function(e){
-    	if ($(this).val().length >= max_chars) { 
-        	$(this).val($(this).val().substr(0, max_chars));
-    	}
-	});
-
-	$('.input.decimal').keyup( function(e){
-    	if ($(this).val().length >= max_chars) { 
-        	$(this).val($(this).val().substr(0, max_chars));
-    	}
-	});
+	$('input.decimal').keypress(function(e) {
+	    var tval = $('textarea').val(),
+	        tlength = tval.length,
+	        set = 5,
+	        remain = parseInt(set - tlength);
+	    $('p').text(remain);
+	    if (remain <= 0 && e.which !== 0 && e.charCode !== 0) {
+	        $('textarea').val((tval).substring(0, tlength - 1))
+	    }
+	})
 
 });
 
