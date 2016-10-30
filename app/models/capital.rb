4341307e3098e6ec5d37ecf3bf9b4bd151b01604
Elliott Blatt <elliott.blatt@gmail.com>
2016-10-30 13:16:12 -0700

    more navigation cleanup

--- app/models/capital.rb
+++ app/models/capital.rb
@@ -3,5 +3,4 @@ class Capital < ApplicationRecord
   has_many :capital_industry_classifications
   has_many :industry_classifications, through: :capital_industry_classifications, class_name: 'IndustryClassification'
   accepts_nested_attributes_for :capital_industry_classifications
-
 end
