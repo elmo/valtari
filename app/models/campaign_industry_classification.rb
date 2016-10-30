afa427f5187407f3339505a4b8ab55bcd435c607
Elliott Blatt <elliott.blatt@gmail.com>
2016-10-30 13:28:38 -0700

    fix campaign, capital required by default

--- app/models/campaign_industry_classification.rb
+++ app/models/campaign_industry_classification.rb
@@ -1,5 +1,5 @@
 class CampaignIndustryClassification < ApplicationRecord
-   belongs_to :campaign
+   belongs_to :campaign, optional: true
    belongs_to :industry_classification
    accepts_nested_attributes_for :industry_classification, :reject_if => :all_blank
 end
--- app/models/capital_industry_classification.rb
+++ app/models/capital_industry_classification.rb
@@ -1,5 +1,5 @@
 class CapitalIndustryClassification < ApplicationRecord
-   belongs_to :capital
-   belongs_to :industry_classification
+   belongs_to :capital, optional: true
+   belongs_to :industry_classification, optional: true
    accepts_nested_attributes_for :industry_classification, :reject_if => :all_blank
 end
