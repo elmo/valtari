4e30536789dfc93559743d6aa19cd8c1c49835ab
Elliott Blatt <elliott.blatt@gmail.com>
2016-12-10 12:13:53 -0800

    fixed migration

--- db/migrate/20161210192636_add_geo_to_business.rb
+++ db/migrate/20161210192636_add_geo_to_business.rb
@@ -2,6 +2,6 @@ class AddGeoToBusiness < ActiveRecord::Migration[5.0]
   def change
     add_column :businesses, :geo_id, :integer
     add_index(:businesses, :geo_id)
-    Business.all.save
+    Business.all.each {|b| b.save }
   end
 end
