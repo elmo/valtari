477976a82d438cbc2af7fe7cd27ecb481d736cc8
Elliott Blatt <elliott.blatt@gmail.com>
2016-12-11 09:49:42 -0800

    some basic tree traversial methods

--- app/helpers/geos_helper.rb
+++ app/helpers/geos_helper.rb
@@ -1,4 +1,25 @@
 module GeosHelper
+
+  def division1_names
+    Geo.select(:division1).distinct.collect(&:division1)
+  end
+
+  def names_within_division1(division1_name)
+    Geo.within_division1(division1_name).collect(&:division2)
+  end
+
+  def names_within_division2(division2_name)
+    Geo.within_division2(division2_name).collect(&:division3)
+  end
+
+  def names_within_division3(division3_name)
+    Geo.within_division3(division3_name).collect(&:division4)
+  end
+
+  def names_within_division4(division4_name)
+    Geo.within_division4(division4_name).collect(&:division5)
+  end
+
   def state_abbrev_map
     {
       AK: "Alaska",
