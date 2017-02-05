a55ff1d8a6961872d84abb20b7879afb266e9ae0
Elliott Blatt <elliott.blatt@gmail.com>
2017-02-05 07:54:49 -0800

    allow users to tag events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :user_id
   scope :future, -> (date) { where(["start_time >= ? ", date ]) }
+  acts_as_ordered_taggable_on :tags
 end
