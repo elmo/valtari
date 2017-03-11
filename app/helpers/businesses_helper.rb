96e8471dd4fcdf17c2ba18e64bab265687623f63
Elliott Blatt <elliott.blatt@gmail.com>
2017-03-11 13:54:19 -0800

    hide/show search fields

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -25,7 +25,7 @@ module BusinessesHelper
     [
      :with_email, :with_phone, :with_address, :with_city, :with_state, :with_country,
      :with_contact_first_name, :with_contact_last_name, :with_contact_title, :with_website,
-     :with_revenue, :with_number_of_employees, :with_naics_code, :with_sic_code, :with_industry_classification,
+     :with_revenue, :with_number_of_employees, :with_naics_code, :with_sic_code,
      :with_parent_company, :with_geo_id, :with_division1, :with_division2, :with_division3,
      :with_division4, :with_division5
      ]
@@ -36,14 +36,29 @@ module BusinessesHelper
      :without_email, :without_phone, :without_address, :without_city,
      :without_state, :without_country, :without_contact_first_name, :without_contact_last_name,
      :without_contact_title, :without_website, :without_revenue, :without_number_of_employees,
-     :without_naics_code, :without_sic_code, :without_industry_classification, :without_parent_company,
+     :without_naics_code, :without_sic_code, :without_parent_company,
      :without_geo_id, :without_division1, :without_division2, :without_division3,
      :without_division4, :without_division5
     ]
   end
 
+   def business_search_fields
+     [:company_name, :city, :state, :postal_code, :country]
+   end
+
+   def business_search_active
+      business_search_fields.each do |field|
+       return 'active' if params[field].present?
+     end
+     return ''
+   end
+
+  def all_business_scopes
+    [with_filters + without_filters].flatten
+  end
+
    def advanced_search_active
-     [with_filters + without_filters].flatten.each do |field|
+     all_business_scopes.each do |field|
        return 'active' if params[field].present?
      end
      return ''
