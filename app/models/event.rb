0be8697a03ccdfbfbd76748481d1785b67dd32da
Peter Tschann <ptgrimm@gmail.com>
2017-03-23 14:35:09 -0700

    event form validation needs more work. when you submit before all valid, seems to break

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
   belongs_to :industry_classification
   validates :title, presence: true
   validates :location, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates_presence_of :user_id
   validates_presence_of :location
   scope :future, -> (date) { where(["start_date >= ? ", date ]) }
