bb11e5e432b721bd25ac8a581cc2b486269b50e3
Peter Tschann <ptgrimm@gmail.com>
2017-03-30 15:59:32 -0700

    validation still needs work

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,9 @@ class Event < ApplicationRecord
   belongs_to :industry_classification
   validates :title, presence: true
   validates :location, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
   validates_presence_of :user_id
-  validates_presence_of :location
   scope :future, -> (date) { where(["start_date >= ? ", date ]) }
   acts_as_ordered_taggable_on :tags
   mount_uploader :photo, PhotoUploader
