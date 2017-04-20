9490e8385d0c4c2ed93f485203bc65a4f394cbea
Elliott Blatt <elliott.blatt@gmail.com>
2017-04-20 10:11:33 -0700

    turn off http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "valtari", password: "interestingdude"
+  #http_basic_authenticate_with name: "valtari", password: "interestingdude"
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :resource_name, :resource, :devise_mapping
