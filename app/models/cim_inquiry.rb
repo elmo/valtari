06c06d91ecdf750eb4e3357234cba7313f379f7c
Elliott Blatt <elliott.blatt@gmail.com>
2017-05-29 20:47:13 -0700

    email scott after inquiry is sumbitted

--- app/models/cim_inquiry.rb
+++ app/models/cim_inquiry.rb
@@ -3,4 +3,12 @@ class CimInquiry < ApplicationRecord
   validates_presence_of :email
   validates_presence_of :phone
   validates_format_of :email, with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  after_create :notify_admin
+
+  private
+
+  def notify_admin
+   CimMailer.notify_admin(cim_inquiry: self).deliver
+  end
+
 end
