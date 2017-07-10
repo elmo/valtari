56c29cbcee3726dd69eb4bade9091281de3ad65b
Elliott Blatt <elliott.blatt@gmail.com>
2017-07-09 21:32:51 -0700

    allow different relationships with a deal room

--- app/models/deal_room.rb
+++ app/models/deal_room.rb
@@ -6,11 +6,12 @@ class DealRoom < ApplicationRecord
   has_many :deal_room_authorizations
   has_many :deal_room_invitations
   has_many :deal_room_activities
-
   after_create :create_deal_room_authorization
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
   accepts_nested_attributes_for :deal_room_uploads
+
+  resourcify
 
   def to_param
     slug
